fc8c744544309bc29ad48a7eb47be7386fe44238
chenjumin <chenjumin@163.com>
2019-11-14 23:41:43 +0800

    1

--- seasy-docker-agent/src/test/java/com/seasy/docker/agent/test/ThriftClientTest.java
+++ seasy-docker-agent/src/test/java/com/seasy/docker/agent/test/ThriftClientTest.java
@@ -2,6 +2,8 @@ package com.seasy.docker.agent.test;
 
 import com.seasy.docker.common.thrift.ClientBootstrap;
 import com.seasy.docker.common.thrift.api.CommandExecutor;
+import com.seasy.docker.common.utils.JsonUtil;
+import com.seasy.docker.common.utils.StringUtil;
 
 public class ThriftClientTest {
 	private static ClientBootstrap clientBootstrap;
@@ -14,6 +16,15 @@ public class ThriftClientTest {
 			
 			client = clientBootstrap.getServiceClient(CommandExecutor.Client.class);
 			String result = client.execute("GET", "", "/images/json");
+			result = StringUtil.trimToEmpty(result);
+
+			
+			if(result.startsWith("{")){
+				result= JsonUtil.object2string(JsonUtil.string2object(result));
+			}else if(result.startsWith("[")){
+				result = JsonUtil.array2string(JsonUtil.string2array(result));
+			}
+			
 			System.out.println(result);
 			
 		}catch(Exception ex){
