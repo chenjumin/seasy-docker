ccddff24bcfccc07c961c60ed6d2fa2d1d642145
chenjumin <chenjumin@163.com>
2019-11-14 23:46:17 +0800

    1

--- seasy-docker-build.sh
+++ seasy-docker-build.sh
@@ -7,6 +7,9 @@ mvn clean assembly:assembly -Ddockerfile.tag.skip
 
 docker rm -f seasy-docker-agent
 
-docker run -d --rm --name seasy-docker-agent -v /var/run/docker.sock:/var/run/docker.sock 192.168.134.139/seasy/seasy-docker-agent:0.0.1
+#docker run -d --rm --name seasy-docker-agent -v /var/run/docker.sock:/var/run/docker.sock 192.168.134.139/seasy/seasy-docker-agent:0.0.1
+docker run -d --name seasy-docker-agent -v /var/run/docker.sock:/var/run/docker.sock -p 5566:5566 192.168.134.139/seasy/seasy-docker-agent:0.0.1
+
+sleep 3
 
 docker ps -a
