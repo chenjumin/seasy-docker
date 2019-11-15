2053f37d0eab914a40e681db4cb42afebb3f1a72
chenjumin <chenjumin@163.com>
2019-11-15 18:10:34 +0800

    1

--- seasy-docker-common/src/main/java/com/seasy/docker/common/mina/AbstractClientChainedHandler.java
+++ seasy-docker-common/src/main/java/com/seasy/docker/common/mina/AbstractClientChainedHandler.java
@@ -1,12 +1,12 @@
 package com.seasy.docker.common.mina;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.mina.core.session.IoSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.seasy.docker.common.mina.config.ClientConfig;
 import com.seasy.docker.common.mina.core.Client;
+import com.seasy.docker.common.utils.StringUtil;
 
 public abstract class AbstractClientChainedHandler extends AbstractChainedHandler {
 	private static Logger logger = LoggerFactory.getLogger(AbstractClientChainedHandler.class);
@@ -24,7 +24,7 @@ public abstract class AbstractClientChainedHandler extends AbstractChainedHandle
 	
 	@Override
 	public void exceptionCaught(IoSession session, Throwable ex) throws Exception {
-		String errorMsg = StringUtils.trimToEmpty(ex.toString());
+		String errorMsg = StringUtil.trimToEmpty(ex.toString());
 		if(errorMsg.matches(".+强迫关闭.+") || errorMsg.matches(".+Connection reset.+")){
 			logger.error("Connection reset：" + ex.toString());
 			session.closeNow();
